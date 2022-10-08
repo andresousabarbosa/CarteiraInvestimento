412654af9fefe17167749fe3ca61e982439f0a60
Andre Barbosa <andre.sousa.barbosa@gmail.com>
2022-10-08 14:57:38 -0300

    Ajuste de separação do menu e inclusão dos arquivos para cadastro do usuario

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, register
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home')
+    path('/accounts/register', register, name='register')
 ]
